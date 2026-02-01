8e993217a391a335943eec18e6c4c65481b737d7
CrayzDev <zohaki247@gmail.com>
2026-02-01 17:22:13 -0500

    Create Config.lua

---
+++ Services/Data/Config.lua
@@ -0,0 +1,14 @@
+return {
+	--  Customize ranges and walk speeds here
+	["Ranges"] = {
+		VisionRange = 50,
+		VisionDetectionRange = 30,
+		AttackRange = 3,
+		EscapeRange = 25,
+	},
+	
+	["Speeds"] = {
+		WalkSpeed = 16,
+		RunSpeed = 16,
+	},
+}
